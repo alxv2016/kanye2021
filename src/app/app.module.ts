f726c8c7057b5ea80c53899acb356356b24ce23a
Alex Vong <avong2011@gmail.com>
2021-01-10 17:34:19 -0500

    fix: refactor to ngrx schematic generator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
+import {StoreModule} from '@ngrx/store';
+import {reducers, metaReducers} from './store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +15,8 @@ import {environment} from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
     StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
   ],
   providers: [],
